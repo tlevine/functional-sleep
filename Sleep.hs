d220dd378e9a339ec345b02258e9d377c25f0820
Thomas Levine <_@thomaslevine.com>
2014-04-05 16:14:13 +0000

    -

--- Sleep.hs
+++ Sleep.hs
@@ -1,12 +1,16 @@
 module Sleep where
 
 import Language.Haskell.TH
+import Control.Concurrent (threadDelay)
 
 cnst :: Int -> String -> Q Exp
 cnst n s = return (LamE (replicate n WildP) (LitE (StringL s)))
 
 --sleep :: Int -> Q Exp
 --sleep = return (LamE [WildP] (LamE ()))
+
+heat :: Integer -> Integer
+heat temperature = 2 ^ temperature
 
 sleep1 :: Q Exp
 sleep1 = return (LamE [] (LitE $ IntegerL $ 2 ^ 10000))
